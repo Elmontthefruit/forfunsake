1131c3553b1624fff526308aa20f506577177a75
Osmel <iamthesexiestfruit@aol.com>
2024-10-28 11:44:53 +0100

    cosis de fresi

---
+++ javascript/menuscripts.js
@@ -0,0 +1,8 @@
+// fetch("texto")
+//     .then(r => r.text())
+//     .then(text => {
+//     document.getElementById("textoScroll").innerText=text.split("/n");
+//     })
+//     .catch((e) => console.error(e));
+
+//¿como coño se lee un txt en JS?!
