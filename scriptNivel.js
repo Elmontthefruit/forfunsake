4fa54c85d16db48cd79b645916fbe3131e267b8d
Osmel <iamthesexiestfruit@aol.com>
2024-10-31 13:19:54 +0100

    me estoy volviendo lo-co

--- scriptNivel.js
+++ scriptNivel.js
@@ -8,7 +8,7 @@ let correcto=false;
 //array de tiempos de alerta
 let indexT=0;
 let tiempoAlerta=["00:05","00:10","00:15","00:19","00:22","00:25","00:30","00:35","00:40","00:42","00:49","00:51","00:53","00:55","00:57","00:57","00:57","00:57","00:57","00:57","00:57","00:57","00:57","01:20","01:20","01:20","01:20","01:20","01:20","01:20","01:34",]
-let tiempoAccion=["00:07","00:12","00:17","00:21","00:23","00:26","00:32","00:36","00:41","00:42","00:50","00:51","00:52","00:55","00:58","01:00","01:01","01:02","01:05","01:09","01:12","01:14","01:16","01:21","01:24","01:25","01:26","01:28","01:30","01:34"]
+let tiempoAccion=["00:07:01","00:12","00:17","00:21","00:23","00:26","00:32","00:36","00:41","00:42","00:50","00:51","00:52","00:55","00:58","01:00","01:01","01:02","01:05","01:09","01:12","01:14","01:16","01:21","01:24","01:25","01:26","01:28","01:30","01:34"]
 let tiempoFin= "1:38";
 let longAlerta= tiempoAlerta.length;
 let longAccion= tiempoAccion.length;
@@ -39,7 +39,9 @@ setInterval( function(){
     document.getElementById("milisec").innerHTML=pad(sec%2);
     document.getElementById("seconds").innerHTML=Math.trunc(pad(++sec%60)/2);
     document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    let tiempoenVar= document.getElementById("minutes").innerText+':'+document.getElementById("seconds").innerText;
+    document.getElementById("wololo").innerHTML=tiempoAccion[indexT]
+    let tiempoenVar= document.getElementById("minutes").innerText+':'+document.getElementById("seconds").innerText+':'+document.getElementById("milisec").innerText;
+    document.getElementById("wololo".innerText=tiempoenVar)
     audioNivel.onended = function(){alert="El nivel ha terminado! Tu puntuación es de "+puntuacion;};
     if (tiempoAccion[indexT]==tiempoenVar){
         $("#bton-1").addClass("dale");
@@ -87,11 +89,13 @@ function pulsaBoton() {
 //extraer tiempo del contador
 
 function dametiempos(){
+let tiempoenmili=document.getElementById("milisec").innerText;
 let tiempoensecs= document.getElementById("seconds").innerText;
 let tiempoenmins=document.getElementById("minutes").innerText;
+
 //Creo que lña alerta para el contador!
 //alert(tiempoenmins+'minutos y '+tiempoensecs+' segundos');
-document.getElementById('metemeaqui').innerText = "<td>"+tiempoenmins+":"+tiempoensecs+"</td>";
+document.getElementById('metemeaqui').innerText = "<td>"+tiempoenmins+":"+tiempoensecs+":"+tiempoenmili+"</td>";
 
 
 }
