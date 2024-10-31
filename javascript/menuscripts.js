40de7f41133f064ab97d73431544d6e6bc75dce9
Osmel <iamthesexiestfruit@aol.com>
2024-10-31 11:26:37 +0100

    mas submodales

--- javascript/menuscripts.js
+++ javascript/menuscripts.js
@@ -10,8 +10,9 @@
 let contenidoMod=document.getElementById("infoModal");
 
 function cambiarModal(idseccion){
-    document.getElementById("infoModal").innerHTML="Esta es mi info sobre cada seccion!.Esta es la seccion numero "+idseccion;
+    document.getElementById("infoModal").innerHTML="Esta es mi info sobre cada seccion!.Esta es la seccion numero "+idseccion+'<br><a href="JavaScript:cambiarModalEspecifico(1)">Clíckame para llamar al submodal</a>';
+    // document.getElementById("infoModal").innerHTML+'<a href="JavaScript:cambiarModalEspecifico(1)">Clíckame para llamar al submodal</a>'
 }
 function cambiarModalEspecifico(idsubsecc){
-    document.getElementById("infoModal").innerHTML="Esta es mi info sobre cada canciones.Esta es la subseccion "+ idsubsecc;
+    document.getElementById("infoModalEspecifico").innerHTML="Esta es mi info sobre cada canciones.Esta es la subseccion "+ idsubsecc;
 }
