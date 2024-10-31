2de79b25e550bb3e4ca1e490f33d6587a3570a2d
Osmel <iamthesexiestfruit@aol.com>
2024-10-31 11:15:56 +0100

    modales empezados

--- javascript/menuscripts.js
+++ javascript/menuscripts.js
@@ -6,3 +6,12 @@
 //     .catch((e) => console.error(e));
 
 //¿como coño se lee un txt en JS?!
+
+let contenidoMod=document.getElementById("infoModal");
+
+function cambiarModal(idseccion){
+    document.getElementById("infoModal").innerHTML="Esta es mi info sobre cada seccion!.Esta es la seccion numero "+idseccion;
+}
+function cambiarModalEspecifico(idsubsecc){
+    document.getElementById("infoModal").innerHTML="Esta es mi info sobre cada canciones.Esta es la subseccion "+ idsubsecc;
+}
