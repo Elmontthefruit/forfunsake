361f176a64d5c7cdfaac136e210b91ecc93fb7c4
Osmel <iamthesexiestfruit@aol.com>
2024-11-30 20:35:33 +0100

    paso1nivel

--- a_escala.php
+++ a_escala.php
@@ -4,37 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="javascript/jquery-3.7.1.min.js"></script>
+    <script src="./javascript/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="niveles/css/a_escala.css">
     <script src="niveles/scripts/a_escala.js"></script>
 
 </head>
-<body>
+<body class="reg_registro ">
     
-
-<section id="infoCancion ">
+<center>
+<section class="" id="infoCancion ">
+   
 <b>Puntuacion</b>
 
 <div id="puntitos">0</div>
+
 </section>
 
 <!-- Aqui se empieza el nivel.OSMEL ACUERDATE DE PONER QUE EL BOTON DESAPAREZCA! -->
-<div class="empezar">
+<div class="empezar ">
 
 
     <audio id="audioCancion">
 
     </audio>
     
-    <button class="btn" id="btnStart" onclick="playAudio()" >Play</button>
+    <button class="btn bg-warning" id="btnStart" onclick="playAudio()" >Play</button>
     <!-- debug -->
     <!-- <div class="visually-hidden"><span id="minutes"></span>:<span id="seconds"></span></div> -->
 </div>
+</center>
 
-
-<section id="juego">
+<section class="" id="juego">
 <div class="container">
     <div class="row bg-success text-center">
         <div class="col-6 offset-2 bg-danger ">
